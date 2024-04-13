44eebcec394f96f4e135e13c6e40e3ea0041ad35
Mayank-SDE <mayankchoudhary6055@gmail.com>
2024-04-13 10:34:36 +0530

    Navigation bar complete

--- EcommerceWebApplication/MKShop/src/App.tsx
+++ EcommerceWebApplication/MKShop/src/App.tsx
@@ -6,7 +6,14 @@ function App() {
   return (
     <>
       <Navbar />
-
+      <section className="flex flex-column justify-center  h-[calc(100vh-50px)] bg-[#ff407d]">
+        <div className="flex flex-start my-20">
+          <h1 className="font-sans italic text-5xl text-white">
+            "From Tech to Trends, Everything at Your Fingertips!"</h1>
+        </div>
+        <div></div>
+        <div></div>
+      </section>
     </>
   )
 }
--- EcommerceWebApplication/MKShop/src/components/Navbar.tsx
+++ EcommerceWebApplication/MKShop/src/components/Navbar.tsx
@@ -1,19 +1,21 @@
 import logoImage from '../assets/favicon_io/apple-touch-icon.png';
+import { MdOutlineScreenSearchDesktop } from "react-icons/md";
 const Navbar = () => {
     return (
-        <div className='bg-slate-950 text-white flex justify-between h-[60px]'>
-            <div className='flex'>
-                <img className='h-8 w-auto' src={logoImage} alt="logo-image" />
-                <div>Search Icon</div>
+        <div className='bg-slate-950 text-white flex justify-between h-[50px] items-center'>
+            <div className='flex items-center px-10'>
+                <img className='h-8 w-auto mx-5' src={logoImage} alt="logo-image" />
+                <input className='border-2 border-blue-500 rounded-xl bg-white cursor-text pl-3 text-black' placeholder="Search..." type="text" />
+                <a href=""> <MdOutlineScreenSearchDesktop className='pl-2 h-8 w-auto' /></a>
             </div>
             <div>
-                <ul className='flex justify-around'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Shop</li>
-                    <li>Help</li>
-                    <li>Your Cart</li>
-                </ul>
+                <div className='flex mr-20 items-center'>
+                    <a href="#" className="px-8">Home</a>
+                    <a href="#" className="px-8">About</a>
+                    <a href="#" className="px-8">Shop</a>
+                    <a href="#" className="px-8">Help</a>
+                    <a href="#" className="px-8 bg-[#ff407d] rounded-full">Your Cart</a>
+                </div>
             </div>
         </div>
     )
